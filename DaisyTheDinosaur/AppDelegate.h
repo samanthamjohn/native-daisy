5eff55fdd910385bd937965bcfc239b174450b83
Samantha John <samantha.m.john@gmail.com>
2012-05-06 18:09:56 -0400

    Initial Commit

---
+++ DaisyTheDinosaur/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DaisyTheDinosaur
+//
+//  Created by Samantha John on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DaisyTheDinosaur/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DaisyTheDinosaur
+//
+//  Created by Samantha John on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
