379dbe2605f323d872f155b5e3ed1d4901e6e8db
Samantha John <samantha.m.john@gmail.com>
2012-05-22 15:39:57 -1000

    Add CALayers.

--- DaisyTheDinosaur/FreeplayViewController.h
+++ DaisyTheDinosaur/FreeplayViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 #import "UIColor+daisy.h"
 #import "MethodView.h"
 #import "ProgramView.h"
--- DaisyTheDinosaur/StageView.m
+++ DaisyTheDinosaur/StageView.m
@@ -6,17 +6,28 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "DaisyGreenView.h"
+#import "StageView.h"
 #import "UIColor+daisy.h"
 
-@implementation DaisyGreenView
+@implementation StageView
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     self = [super initWithCoder:aDecoder];
     if (self) {
         self.backgroundColor = [UIColor daisyGreenColor];
+        CALayer *backgroundLayer = [CALayer layer];
+        backgroundLayer.bounds = CGRectMake(0.f, 0.f, 963.f, 247.f);
+        backgroundLayer.position = CGPointMake(494.f, 136.f);
+        NSString* imageFileName = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"daisy-bg.jpg"];
+        CGDataProviderRef dataProvider = CGDataProviderCreateWithFilename([imageFileName UTF8String]);
+        CGImageRef backgroundImg = CGImageCreateWithJPEGDataProvider(dataProvider, NULL, NO, kCGRenderingIntentDefault);
+        backgroundLayer.contents = (__bridge_transfer id) backgroundImg;
+        [self.layer addSublayer:backgroundLayer];
+        CGDataProviderRelease(dataProvider);
+
     }
     return self;
-}
+}   
+
 @end
